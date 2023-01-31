dafae4a17ad77d7999994a019be02aef5fd57958
baxter08 <baxter08@hotmail.fr>
2023-01-31 11:27:09 +0100

    ajoute message User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,8 +6,11 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-
-
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+#[UniqueEntity(
+    'email',
+    message : 'Cette e-mail existe déjà')]
+    
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
