8dbe02c6af023dfadc851e15b27cede5a5463e3f
baxter08 <baxter08@hotmail.fr>
2023-02-01 10:51:45 +0100

    changer float a decimal ORM

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\ArticleRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\DecimalType;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -23,7 +24,7 @@ class Article
     private ?string $description = null;
 
     #[ORM\Column]
-    private ?float $prix = null;
+    private ?DecimalType $prix = null;
 
     #[ORM\OneToMany(mappedBy: 'Article', targetEntity: Image::class)]
     private Collection $images;
