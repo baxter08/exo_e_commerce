7bc76f0a7dabf6b45282c5c9bf79767ea2796364
baxter08 <baxter08@hotmail.fr>
2023-02-03 08:58:02 +0100

    PAGE ARTICLE

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('slider', './assets/js/slider.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -63,7 +64,7 @@ Encore
              to: 'images/[path][name].[ext]',
     
              // if versioning is enabled, add the file hash too
-             to: 'images/[path][name].[hash:8].[ext]',
+            //  to: 'images/[path][name].[hash:8].[ext]',
     
              // only copy files matching this pattern
              pattern: /\.(png|jpg|jpeg)$/
