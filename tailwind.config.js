bbbfc69a14132094beeddef13f313fc5cb52cfd2
baxter08 <98820950+baxter08@users.noreply.github.com>
2023-04-26 23:35:58 +0200

    accueil corection beug

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
     './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
+    colors: {
+      indigo: '#5c6ac4',
+      blue: '#007ace',
+      red: '#de3618',
+      gray: {
+        '100': '#f5f5f5',
+        '200': '#eeeeee',
+        '300': '#e0e0e0',
+        '400': '#bdbdbd',
+        '500': '#9e9e9e',
+        '600': '#757575',
+        '700': '#616161',
+        '800': '#424242',
+        '900': '#212121',
+      }
+    },
     extend: {},
   },
   plugins: [require("tw-elements/dist/plugin")],
