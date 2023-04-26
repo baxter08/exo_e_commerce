dd62c45019a66b5ee9024b8643d8cb10c170eaa0
baxter08 <98820950+baxter08@users.noreply.github.com>
2023-04-27 01:03:06 +0200

    front et container et slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       indigo: '#5c6ac4',
       blue: '#007ace',
       red: '#de3618',
+      white: '#ffffff',
       gray: {
         '100': '#f5f5f5',
         '200': '#eeeeee',
