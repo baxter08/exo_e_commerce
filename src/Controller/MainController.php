38cc83405fb07a09a8181f4c5a6072c20f0eb295
baxter08 <baxter08@hotmail.fr>
2023-04-26 17:35:44 +0200

    changement

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -14,34 +14,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class MainController extends AbstractController
 {
 
-    
+
     #[Route('/', name: 'app_index')]
-    public function index(ArticleRepository $articleRepos,Request $request): Response
+    public function index(ArticleRepository $articleRepos, Request $request): Response
     {
-       
+
         $form = $this->createForm(SearchType::class);
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
 
             // dd($form->get('q')->getData());
 
             // on recherche les article coresponndant au mots de la recherche
             $articles = $articleRepos->search($form->get('q')->getData());
-
-        }
-        else{
+        } else {
             $articles = $articleRepos->findAll();
         }
         // $articles = $articleRepos->findAll();
-        
-        return $this->render('pages/main/index.html.twig', [
-            'articles' => $articles ,
+
+        return $this->render('pages/main/accueil.html.twig', [
+            'articles' => $articles,
             'form' => $form->createView()
-          
+
         ]);
-  
     }
 
     // #[Route('/', name: 'ajax')]
@@ -59,34 +56,29 @@ class MainController extends AbstractController
     //     ]);
 
     // }
-    
-    
+
+
     #[Route('/', name: 'search_text')]
-    public function search(ArticleRepository $articleRepo, Request $request,
+    public function search(
+        ArticleRepository $articleRepo,
+        Request $request,
     ): Response {
         //  dd($request);
-    
-        if($request) {
+
+        if ($request) {
 
             $articles = $articleRepo->search($request->get('mots'));
-
-      
-        }else{
+        } else {
 
             $articles = $articleRepo->findAll();
-
         }
-            
+
         return $this->render('article/index.html.twig', [
             'controller_name' => 'jp',
             'articles' => $articles
-           
-        
-            
+
+
+
         ]);
-
     }
-    
 }
-
-
